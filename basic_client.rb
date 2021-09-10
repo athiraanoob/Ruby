85a120be327d57d7ba92fb0d4e605d7e90a17858
athiraanoob <50247736+athiraanoob@users.noreply.github.com>
2021-09-10 11:04:06 -0400

    delete

--- basic_client.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'bot'
-bot = Bot.new(name: "Botty", data_file: "botty.bot")
-puts "plaese enter goodbye to stop conversation"
-puts bot.greeting
-while input = gets and input.chomp != 'goodbye'
-  puts ">> " + bot.response_to(input)
-end
-puts bot.farewell
